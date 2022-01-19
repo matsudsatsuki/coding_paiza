ce5972340183170ac5bffb93ce95551bb6ccdda4
matsudasatsuki <yk.xxx.org@gmail.com>
2022-01-19 15:01:41 +0900

    add map boss

--- a_rank_snake_map_move.py
+++ a_rank_snake_map_move.py
@@ -33,3 +33,43 @@ for t_now in range(100):
         break
     else:
         print(sy,sx)
+
+#map move boss 
+H,W,sy,sx,N = map(int,input().split())
+S = [list(input())for i in range(H)]
+time_lr = [input().split() for i in range(N)]
+#print(time_lr)
+
+directions = ['N','E','S','W']
+sy = int(sy)
+sx = int(sx)
+now_directions = 0
+time_index = 0
+S[sy][sx] = '*'
+
+for t_now in range(100):
+    if time_index < N and str(t_now) == time_lr[time_index][0]:
+        d = time_lr[time_index][1]
+        time_index += 1
+        if d == 'L':
+            now_directions = (3+now_directions)%4  
+        else:
+            now_directions = (1+now_directions)%4
+    
+    if directions[now_directions] == 'N':
+        sy -= 1
+    elif directions[now_directions] == 'E':
+        sx += 1
+    elif directions[now_directions] == 'S':
+        sy += 1
+    elif directions[now_directions] == 'W':
+        sx -= 1 
+    if sx < 0 or sx >= int(W) or sy < 0 or sy >= int(H) or S[sy][sx] == '#' or S[sy][sx] != '.':
+        break
+    else:
+        S[sy][sx] = '*'
+
+for y in range(int(H)):
+    for x in range(int(W)):
+        print(S[y][x], end='')
+    print()      
