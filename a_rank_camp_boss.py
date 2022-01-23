6ec36744a875fdedee2e5d0515159534b9b4a5c5
matsudasatsuki <yk.xxx.org@gmail.com>
2022-01-23 16:12:20 +0900

    add breadth first search

--- a_rank_camp_boss.py
+++ a_rank_camp_boss.py
@@ -173,3 +173,61 @@ for y in range(int(H)):
     for x in range(int(W)):
         print(s[y][x], end="")
     print()
+
+#practice_3 submit
+H,W = map(int,input().split())
+
+S = [list(input())for i in range(H)]
+flag = False
+for y in range(H):
+    for x in range(W):
+        if S[y][x] == '*':
+            if y > 0:
+                if S[y-1][x] != '#':
+                    S[y-1][x] = '*'
+            if y < H-1:
+                if S[y+1][x] != '#':
+                    S[y+1][x] = '*'
+            if x > 0:
+                if S[y][x-1] != '#':
+                    S[y][x-1] = '*'
+            if x < W-1:
+                if S[y][x+1] != '#':
+                    S[y][x+1] = '*'
+            
+    
+
+for i in range(H):
+    print(''.join(S[i]))
+            
+#answer
+H, W = map(int, input().split())
+s = [list(input()) for _ in range(H)]
+my_p = []
+
+for y in range(H):
+    for x in range(W):
+        if s[y][x] == "*":
+            my_p.append([y, x])
+
+while my_p:
+    [y, x] = my_p.pop(0)
+
+    if y > 0 and s[y - 1][x] == ".":
+        s[y - 1][x] = "*"
+        my_p.append([y - 1, x])
+    if y < H - 1 and s[y + 1][x] == ".":
+        s[y + 1][x] = "*"
+        my_p.append([y + 1, x])
+    if x > 0 and s[y][x - 1] == ".":
+        s[y][x - 1] = "*"
+        my_p.append([y, x - 1])
+    if x < W - 1 and s[y][x + 1] == ".":
+        s[y][x + 1] = "*"
+        my_p.append([y, x + 1])
+
+for y in range(int(H)):
+    for x in range(int(W)):
+        print(s[y][x], end="")
+    print()
+    
