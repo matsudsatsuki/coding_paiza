05b911e5e7fbe362a62da84d6c6c35d92db943e6
matsudasatsuki <yk.xxx.org@gmail.com>
2022-01-23 16:12:59 +0900

    add my answer camp

--- a_rank_camp_boss.py
+++ a_rank_camp_boss.py
@@ -230,4 +230,64 @@ for y in range(int(H)):
     for x in range(int(W)):
         print(s[y][x], end="")
     print()
+
+
+#final_problem submit
+H,W = map(int,input().split())
+first_p = input()
+
+S = [list(input())for i in range(H)]
+count = 0
+aadd = 0
+sums = [1,1]
+flag_pass = True
+ab_point = [[],[]]
+
+if first_p == 'B':
+    count += 1
+    aadd += 1
+    
+for y in range(H):
+    for x in range(W):
+        if S[y][x] == 'A':
+            ab_point[0].append([y,x,aadd])
+        if S[y][x] == 'B':
+            ab_point[1].append([y,x,0])
+
+while len(ab_point[0])>0 or len(ab_point[1])>0:
+    if len(ab_point[count%2]) == 0:
+        count += 1
+        flag_pass = False
+    [y,x,n] = ab_point[count%2][0]
+    
+    if count / 2 < n and flag_pass:
+        count += 1
+        [y,x,n] = ab_point[count%2][0]
+    
+    ab_point[count%2].pop(0)
+    ab = 'A' if count % 2 == 0 else 'B'
+    
+    if y > 0 and S[y-1][x] == '.':
+        S[y-1][x] = ab
+        sums[count%2] += 1
+        ab_point[count%2].append([y-1,x,n+1])
+    if y < H-1 and S[y+1][x] == '.':
+        S[y+1][x] = ab
+        sums[count%2] += 1
+        ab_point[count%2].append([y+1,x,n+1])
+    if x > 0 and S[y][x-1] == '.':
+        S[y][x-1] = ab
+        sums[count%2] += 1
+        ab_point[count%2].append([y,x-1,n+1])
+    if x < W-1 and S[y][x+1] == '.':
+        S[y][x+1] = ab
+        sums[count%2] += 1
+        ab_point[count%2].append([y,x+1,n+1])
+
+print(sums[0],sums[1])
+if sums[0] > sums[1]:
+    print('A')
+else:
+    print('B')
+        
     
