aecd5a28dd449cbe2e62ebfa4d1f0163dce022d9
matsudasatsuki <yk.xxx.org@gmail.com>
2022-01-25 12:10:55 +0900

    add C078(failed)

---
+++ C078.py
@@ -0,0 +1,20 @@
+#failed
+N,c_1,c_2 = map(int,input().split())
+stock = 0
+profit = 0
+close_price = 0
+for day in range(N):
+    price = int(input())
+    if price > c_1 and price < c_2 and day != N-1:
+        break
+    if day == N-1:
+        profit += stock*price
+        break
+    if price <= c_1:
+        stock += 1
+        profit -= price
+    if price >= c_2:
+        profit += stock*price
+        stock = 0
+print(profit)
+    
