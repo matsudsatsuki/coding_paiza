b43f62f6b557be16d3853aaa6c373079b0af8d43
matsudasatsuki <yk.xxx.org@gmail.com>
2022-01-25 12:13:50 +0900

    modified C078

--- C078.py
+++ C078.py
@@ -1,4 +1,4 @@
-#failed
+#failed(continueとbreakを間違えた)
 N,c_1,c_2 = map(int,input().split())
 stock = 0
 profit = 0
@@ -6,7 +6,7 @@ close_price = 0
 for day in range(N):
     price = int(input())
     if price > c_1 and price < c_2 and day != N-1:
-        break
+        break#continueにするとsuccess
     if day == N-1:
         profit += stock*price
         break
